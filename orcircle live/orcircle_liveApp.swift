f8d463b28b8e284a220e4d84dedc09e99c7ca73f
jayVRIY <yuanjie1224@gmail.com>
2022-11-13 02:51:56 +0800

    Initial Commit

---
+++ orcircle live/orcircle_liveApp.swift
@@ -0,0 +1,17 @@
+//
+//  orcircle_liveApp.swift
+//  orcircle live
+//
+//  Created by jay on 2022/11/13.
+//
+
+import SwiftUI
+
+@main
+struct orcircle_liveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
